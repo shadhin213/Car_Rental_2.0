bbc8d8c2f77d6d1a2cc62480562fa83da438f8d4
MD.Shadhin <shadhin.cse71@gmail.com>
2025-09-17 20:36:05 +0600

    modify the the customer dashboard features,

--- obj/Debug/net9.0/CarRentalManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/CarRentalManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRentalManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f0d5196bd685d8f5a9d07da2a1efc9f2c836fcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934b1ebe949013599b1a90a789145774eb3e6a73")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRentalManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRentalManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
